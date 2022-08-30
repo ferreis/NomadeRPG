ec31bfe57d3413150913c087ec5eec4e5ac244a6
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2022-08-30 19:00:49 -0300

    Add files via upload

---
+++ Teste Bahia/main (4).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "jogo.h"
+
+ using namespace std;
+
+ int main()
+ {
+    Personagem P1;
+    IniJogador(P1);
+    Menu(P1);
+
+
+  /*do{
+   cout << "\n \n \n \n \n \n ";
+  } while (sair == false);*/
+ }

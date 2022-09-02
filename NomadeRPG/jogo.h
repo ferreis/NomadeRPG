2f28047575d7ff02b6fbf93add3b1bac17c06d75
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2022-09-02 20:56:54 -0300

    Add files via upload

--- NomadeRPG/jogo.h
+++ NomadeRPG/jogo.h
@@ -1,8 +1,17 @@
+#ifndef JOGO_H_INCLUDED
+#define JOGO_H_INCLUDED
+
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <windows.h>
+
 using namespace std;
 
+ifstream salas;
+fstream save;
+string line;
+int escolha, p = 0, q = 0, c=0;
  //atributos do jogador
 struct Personagem
 {
@@ -24,28 +33,46 @@ struct Equipamento
    string sextra;
    int ipotion;
 };
+struct Fase{
+    int I, F;
+};
+
+void Fases(Fase &s, int &x){
+    s.I = x;
+    x = x + 7;
+    s.F = x;
+}
+
+/*void Nomaderpg(){
+cout << "╔═╦╗────────────╔╗───╔═╗╔═╗╔══╗\n";
+cout << "║║║║╔═╗╔══╗╔═╗─╔╝║╔═╗║╬║║╬║║╔═╣\n";
+cout << "║║║║║╬║║║║║║╬╚╗║╬║║╩╣║╗╣║╔╝║╚╗║\n";
+cout << "╚╩═╝╚═╝╚╩╩╝╚══╝╚═╝╚═╝╚╩╝╚╝─╚══╝\n";
+cout << "───────────────────────────────\n";
+}*/
  //função de inicialização do jogador
 void IniJogador(Personagem &jogador){
-
-   int raca;
-   int classe;
-   cout << "CRIE SEU PERSONAGEM\n\nNome:";
-   cin >> jogador.snome;
-   system("cls");
-   cout << "Escolha a sua Raca: \n 1) Anao \n 2) Elfo \n 3) Halfling\n\nEscolha : \n";
-   cin >> raca;
-   system("cls");
-   while(raca < 1 || raca > 3){
-      cout << "Opcao inexistente, tente novamente:\n";
-      cin >> raca;
-      system("cls");
-      cout << "Escolha a sua Raca: \n 1) Anao \n 2) Elfo \n 3) Halfling\n\nEscolha : \n";
-   }
-   switch(raca){
-      case 1:
+    int raca;
+    int classe;
+    cout << "CRIE SEU PERSONAGEM\n\nNome:";
+    cin >> jogador.snome;
+    system("cls");
+    //Nomaderpg();
+    cout << "Escolha a sua Raca: \n 1) Anao \n 2) Elfo \n 3) Halfling\n\nEscolha : \n";
+    cin >> raca;
+    system("cls");
+    //Nomaderpg();
+        while(raca < 1 || raca > 3){
+        cout << "Opcao inexistente, tente novamente:\n";
+        cin >> raca;
+        system("cls");
+        cout << "Escolha a sua Raca: \n 1) Anao \n 2) Elfo \n 3) Halfling\n\nEscolha : \n";
+    }
+    switch(raca){
+    case 1:
          jogador.ifor+=1;
          jogador.ivida+=1;
-         jogador.sraca = "Anão";
+         jogador.sraca = "Anao";
          break;
       case 2:
          jogador.imagia+=1;
@@ -54,13 +81,14 @@ void IniJogador(Personagem &jogador){
          break;
       case 3:
          jogador.isorte+=2;
-         jogador.idex+=1;
-         jogador.ivida-=1;
+         jogador.idef+=1;
+         jogador.ivida-= 1;
          jogador.sraca = "Halfling";
          break;
    }
    system("cls");
-   cout << "Escolha a Classe: \n 1) Guerreiro \n 2) Mago \n 3) Ladino: \n";
+   //Nomaderpg();
+   cout << "Escolha a Classe: \n 1) Guerreiro \n 2) Mago \n 3) Ladino \n\nEscolha : \n";
    cin>>classe;
    switch (classe) {
       case 1:
@@ -92,41 +120,131 @@ void IniJogador(Personagem &jogador){
          jogador.sclasse = "Aldeao";
          break;
    }
+   system("cls");
 }
 void tela_status(Personagem status){
-    cout<<"╭══════════════════════════════════════════════════════════════════════════╮"<<endl;
-    cout<<"|Nome:"<< status.snome<< " |Classe:"<< status.sclasse<<" |Raça:"<< status.sraca<<"| \n";
-    cout<<"|Força:"<< status.ifor<< "    |Destreza:"<< status.idex<<" |Sorte:"<< status.isorte<<"| \n";
-    cout<<"|Magia:"<< status.imagia<< "    |Vida:"<< status.ivida<<" |Defesa:"<< status.idef<<"| \n";
-    cout<<"╰══════════════════════════════════════════════════════════════════════════╯";
+    cout<<"----------------------------------------------------------------------------"<<endl;
+    cout<<"|Nome: "<< status.snome<< "   |Classe: "<< status.sclasse<<"   |Raca: "<< status.sraca<<"| \n";
+    cout<<"|Forca: "<< status.ifor<< "   |Destreza: "<< status.idex<<"   |Sorte: "<< status.isorte<<"| \n";
+    cout<<"|Vida: "<< status.ivida<<"   |Defesa: "<< status.idef<<"   |Magia: "<< status.imagia<< "|\n";
+    cout<<"----------------------------------------------------------------------------" << endl;
 }
-void IniJogo(Personagem P1){
-   ifstream salas;
-   string line;
-   salas.open("jogo.txt");
-   for (int i = 0; i < 30; i++)
-   {
-      getline(salas, line);
-      if (i<7)
-      {
-         cout << line<<endl;
-      }
-   }
-   
-   salas.close();
+
+void escrever(Personagem &P1, Fase s[][3],int &p, int &q){
+    salas.open("jogo.txt");
+    for(int m=0; m<(s[p][q].F); m++)
+    {
+        getline(salas,line);
+        if(m >= (s[p][q].I)){
+            cout << line<< endl;
+        }
+    }
+    salas.close();
+    cout << "Escolha(1, 2, 3(Menu)) : ";
+    cin >> escolha;
+    system("cls");
+    if(q==0){
+        switch(escolha){
+        case 1:
+            q++;
+            escrever(P1,s,p,q);
+            break;
+        case 2:
+            q+=2;
+            escrever(P1,s,p,q);
+            break;
+        case 3:
+            break;
+        }
+    }
+    else{
+        switch(escolha){
+        case 1:
+            q=0;
+            p++;
+            escrever(P1,s,p,q);
+            break;
+        case 2:
+            q=0;
+            p+=2;
+            escrever(P1,s,p,q);
+            break;
+        case 3:
+            break;
+        }
+    }
 }
-void Menu(Personagem P1, bool &sair){
+
+void salvar(Personagem P1, int p, int q){
+    save.open("save.csv");
+    save << "sep=, \n";
+    save << P1.snome << "\n";
+    save << P1.sclasse << "\n";
+    save << P1.sraca << "\n";
+    save << P1.ifor << "\n";
+    save << P1.idex << "\n";
+    save << P1.isorte << "\n";
+    save << P1.ivida << "\n";
+    save << P1.idef << "\n";
+    save << P1.imagia<< "\n";
+    save << p << "\n" << q;
+    save.close();
+    cout << "Salvamento realizado com sucesso!!\n";
+    Sleep(2000);
+    system("cls");
+}
+
+void carregar(Personagem &P1, int &p, int &q){
+    save.open("save.csv");
+    string load;
+    getline(save, load);
+    getline(save, P1.snome);
+    getline(save, P1.sclasse);
+    getline(save, P1.sraca);
+    getline(save, load);
+    P1.ifor = atoi(load.c_str());
+    getline(save, load);
+    P1.idex = atoi(load.c_str());
+    getline(save, load);
+    P1.isorte = atoi(load.c_str());
+    getline(save, load);
+    P1.ivida = atoi(load.c_str());
+    getline(save, load);
+    P1.idef = atoi(load.c_str());
+    getline(save, load);
+    P1.imagia = atoi(load.c_str());
+    getline(save, load);
+    p = atoi(load.c_str());
+    getline(save, load);
+    q = atoi(load.c_str());
+    save.close();
+    cout << "Carregamento realizado com sucesso!!\n";
+    Sleep(2000);
+    system("cls");
+}
+
+void Menu(Personagem &P1){
+    Personagem P2;
+    Fase s[10][3];
+    int x = 0;
+    for(int i=0; i<10; i++){
+        for(int j=0; j<2; j++){
+            Fases(s[i][j], x);
+        }
+    }
     int espera;
-    string desenhomenu[31][11];
+    int voltar;
+    string desenhomenu[31][13];
     for (int x = 0; x < 31;x++){
-        for (int y = 0; y < 11; y++){
+        for (int y = 0; y < 13; y++){
             if(x==10){
                 desenhomenu[x][1]="            Menu            ";
-                desenhomenu[x][3]="         1- Inicio          ";
+                desenhomenu[x][3]="         1- Jogar           ";
                 desenhomenu[x][5]="         2- Status          ";
                 desenhomenu[x][7]="         3- Salvar          ";
-                desenhomenu[x][9]="         4-  Sair           ";
-            }else if (x==0||y==0||x==30||y==10){
+                desenhomenu[x][9]="         4- Carregar        ";
+                desenhomenu[x][11]="         5- Sair            ";
+            }else if (x==0||y==0||x==30||y==12){
                 desenhomenu[x][y]="#";
             }
             else if(y%2==0){
@@ -136,7 +254,7 @@ void Menu(Personagem P1, bool &sair){
             }
         }
     }
-       for (int y = 0; y < 11;y++){
+    for (int y = 0; y < 13;y++){
         for (int x = 0; x < 31; x++)
         {
               cout << desenhomenu[x][y];
@@ -147,15 +265,51 @@ void Menu(Personagem P1, bool &sair){
     system("cls");
     switch(espera){
     case 1:
+        if(c==0)
+        {
+            IniJogador(P1);
+        }
+        escrever(P1,s,p,q);
+        Menu(P1);
         break;
     case 2:
         tela_status(P1);
         cout << "Para voltar digite 1: ";
+        cin >> voltar;
         system("cls");
-        Menu(P1,sair);
+        Menu(P1);
         break;
-      case 4:
-         sair = true;
-      break;
+    case 3:
+        cout << "Deseja salvar realmente? (1-sim / 2-nao) ";
+        cin >> escolha;
+        system("cls");
+        if(escolha == 1){
+            salvar(P1, p, q);
+        }
+        Menu(P1);
+        break;
+    case 4:
+        cout << "Deseja carregar um salvamento? (1-sim / 2-nao) ";
+        cin >> escolha;
+        system("cls");
+        if(escolha == 1){
+            carregar(P1, p, q);
+        }
+        c++;
+        Menu(P1);
+        break;
+    case 5:
+        break;
+
     }
 }
+/*cout << "Deseja comecar um novo jogo? (1-Sim / 2-Nao) ";
+        cin >> escolha;
+        system("cls");
+        if(escolha == 2){
+            Menu(P1);
+        }
+        else{*/
+
+
+#endif // JOGO_H_INCLUDED
--- NomadeRPG/main.cpp
+++ NomadeRPG/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
 #include "jogo.h"
+
  using namespace std;
-int main(){
-  bool sair = false;
-  int espera;
-  Personagem jogador;
-  IniJogador(jogador);
-  do
-  {
-  IniJogo(jogador);
-  Menu(jogador,sair);
-  system("pause");
-    
-  } while (sair != true);
-}
- 
+
+ int main()
+ {
+    Personagem P1;
+    Menu(P1);
+
+
+
+  /*do{
+   cout << "\n \n \n \n \n \n ";
+  } while (sair == false);*/
+ }

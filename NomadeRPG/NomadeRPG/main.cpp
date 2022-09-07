fa057cbd2b364ff14fd9d7447941bd2e6882fa3a
Rafael Fernando dos reis Mecabô <rafa_ferreis@hotmail.com>
2022-09-07 18:29:35 -0300

    agora vai

---
+++ NomadeRPG/NomadeRPG/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "jogo.h"
+
+using namespace std;
+
+int main()
+{
+    Personagem P1;
+    Menu(P1, f);
+
+
+
+  /*do{
+   cout << "\n \n \n \n \n \n ";
+  } while (sair == false);*/
+}
